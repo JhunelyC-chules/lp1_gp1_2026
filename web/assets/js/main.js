f4640c04d8942f226af0d6c48c08d823eac9ac1d
LAB 2 <LAB 2@LAB2-36>
2026-06-18 14:36:26 -0500

    visualizacion de los productos en el carrito

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -3,4 +3,50 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
  */
 
+async function loadComponent(id, file) {
+    const response = await fetch(file);
+    const data = await response.text();
+    document.getElementById(id).innerHTML = data;
 
+}
+function loadScript(src) {
+    return new Promise((resolve, reject) => {
+        const script = document.createElement('script');
+        script.src = src;
+        script.onload = resolve;
+        script.onerror = reject;
+        document.body.appendChild(script);
+    });
+}
+
+async function init() {
+    try {
+        await loadComponent('head-placeholder', 'head.html');
+        await loadComponent('header-placeholder', 'header.html');
+        await loadComponent('footer-placeholder', 'footer.html');
+
+        await loadScript('assets/js/jquery-3.6.0.min.js');
+        await loadScript('assets/js/jquery.dataTables.min.js');
+        await loadScript('assets/js/dataTables.bootstrap5.min.js');
+
+        await loadScript('assets/js/bootstrap.bundle.min.js');
+        await loadScript('assets/js/sweetalert2.js');
+
+        //cargar los script y funciones
+        await loadScript('assets/js/tienda.js');
+        setTimeout(() => {
+            if (typeof cargarProductos() === 'function')
+                cargarProductos();
+            if (typeof agregarCarrito() === 'function')
+                agregarCarrito();
+            if (typeof actualizarContadorCarrito() === 'function')
+                actualizarContadorCarrito();
+
+        }, 200)
+
+
+    } catch (e) {
+        console.error("Error al cargar la aplicacion", e);
+    }
+}
+init();
